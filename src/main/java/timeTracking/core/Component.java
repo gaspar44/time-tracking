c5dbc034452941594e7a1caf2ba6cc0ad4ee2299
1391570 <91088455+1391570@users.noreply.github.com>
2021-10-16 22:49:54 +0200

    Añadido de archivo de Alex

--- src/main/java/timeTracking/core/Component.java
+++ src/main/java/timeTracking/core/Component.java
@@ -1,19 +1,43 @@
 package timeTracking.core;
 
 import timeTracking.api.Visitor;
-
+import java.awt.*;
+import java.sql.Time;
+import org.json.*;
 import java.util.List;
+
+//Cada clase que dependa de Component deberá ser: public class Name implements Component{}
+//https://www.baeldung.com/java-composite-pattern
 
 public abstract class Component {
   private String name;
+  private String ID;
+  Component Components;
+
   protected List<Component> components;
 
   protected long getTotalTime() {
+
     return 0;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public String getID(){
+    return ID;
+  }
+
+  public void accept(JsonParser visitor) {
+
+
   }
 
   private void acceptVisitor(Visitor visitor) {
 
   }
+
 
 }
