e7753d5ece1646b15a2b5a9c52246c52a649d6b7
Alejandro Sabater 1456140 <alexsb98@gmail.com>
2021-11-22 22:15:11 +0100

    Updated todas las funcionalidades

--- codelab_timetracker/lib/formulario.dart
+++ codelab_timetracker/lib/formulario.dart
@@ -3,35 +3,32 @@ import 'dart:core';
 import 'package:codelab_timetracker/page_activities.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
   _RegisterPageState createState() => _RegisterPageState();
 }
 
-
-
-
-
-
-
 class _RegisterPageState extends State<RegisterPage> {
   GlobalKey<FormState> keyForm = new GlobalKey();
-  TextEditingController  nameCtrl = new TextEditingController();
-  TextEditingController  emailCtrl = new TextEditingController();
-  TextEditingController  mobileCtrl = new TextEditingController();
-  TextEditingController  passwordCtrl = new TextEditingController();
-  TextEditingController  repeatPassCtrl = new TextEditingController();
-
   TextEditingController Period = new TextEditingController();
   TextEditingController From = new TextEditingController();
   TextEditingController To = new TextEditingController();
   TextEditingController Content = new TextEditingController();
   TextEditingController Format = new TextEditingController();
-  DateTime selectedTime = DateTime.now();
 
+  DateTime selectedTimeTo = DateTime.now();
+  DateTime selectedTimeFrom = DateTime.now();
 
+  List<String> _locations = ['Last week', 'This week', 'Yesterday', 'Today', 'Other']; // Option 1
+  String _selectedLocation = 'Last week'; // Option 1
 
+  List<String> _ContentList = ['Brief', 'Detailed', 'Statistic']; // Option 2
+  String _selectedContent = 'Brief'; // Option 2
+
+  List<String> _FormatList= ['Web page', 'PDF', 'Text']; // Option 3
+  String _selectedFormat = 'Web page'; // Option 3
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +47,7 @@ class _RegisterPageState extends State<RegisterPage> {
           ),
         ),
     );
-  }
+    }
 
   formItemsDesign(icon, item) {
     return Padding(
@@ -59,59 +56,102 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
-  String gender = 'hombre';
-
 
   Widget formUI() {
+
+    String _dropDownValue;
+
     return  Column(
-      children: <Widget>[
 
 
-    DropdownButton<String>(
 
-      items: <String>['Last week', 'This week', 'Yesterday', 'Today', 'Other'].map((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
+    children: <Widget>[
+      // Last week
+      Text('Period'),
+      DropdownButton<String>(
+        isExpanded: true,
+        value: _selectedLocation,
+        onChanged: (newValue) {
+          setState(() {
+            _selectedLocation = newValue!;
+            });
+        },
+        items: _locations.map((location) {
+          return DropdownMenuItem(
+            child: new Text(location),
+            value: location,
         );
-      }).toList(),
-      hint: const Text('Last week'),
-      onChanged: (_) {},
-    ),
+        }).toList(),
+      ),
+
+      // Brief
+      Text('Content'),
+      DropdownButton<String>(
+        isExpanded: true,
+        value: _selectedContent,
+        onChanged: (newValue) {
+          setState(() {
+            _selectedContent = newValue!;
+          });
+        },
+        items: _ContentList.map((location) {
+          return DropdownMenuItem(
+            child: new Text(location),
+            value: location,
+          );
+        }).toList(),
+      ),
 
 
-        DropdownButton<String>(
-          hint: const Text('Brief'),
-          items: <String>['Brief', 'Detailed', 'Statistic'].map((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-          onChanged: (_) {},
-        ),
+
+      Text('Format'),
+
+      DropdownButton<String>(
+        isExpanded: true,
+        value: _selectedFormat,
+        onChanged: (newValue) {
+          setState(() {
+            _selectedFormat = newValue!;
+          });
+        },
+        items: _FormatList.map((location) {
+          return DropdownMenuItem(
+            child: new Text(location),
+            value: location,
+          );
+        }).toList(),
+      ),
 
 
-        DropdownButton<String>(
-          hint: const Text('Web page'),
-          items: <String>['Web page', 'PDF', 'Text'].map((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
-          onChanged: (_) {},
-        ),
-
-      Text(selectedTime.toString()),
+      Text(DateFormat('yyyy-MM-dd').format(selectedTimeFrom)),
         RaisedButton(
-          onPressed: () => _selectDate(context)
+            onPressed: () => _selectDate(context)
+        ),
+
+      Text(DateFormat('yyyy-MM-dd').format(selectedTimeTo)),
+        RaisedButton(
+          onPressed: () => _selectFinalDate(context)
         ),],);
 }
 
 
-
   Future<void> _selectDate(BuildContext context) async {
+    DateTime currentDate = DateTime.now();
+
+    final DateTime? pickedDate = await showDatePicker(
+        context: context,
+        initialDate: currentDate,
+        firstDate: DateTime(2015),
+        lastDate: DateTime(2050));
+    if (pickedDate != null && pickedDate != currentDate) {
+      setState(() {
+        selectedTimeFrom = pickedDate;
+        get() => selectedTimeFrom;
+      });
+    }
+  }
+
+  Future<void> _selectFinalDate(BuildContext context) async {
     DateTime currentDate = DateTime.now();
     final DateTime? pickedDate = await showDatePicker(
         context: context,
@@ -120,11 +160,48 @@ class _RegisterPageState extends State<RegisterPage> {
         lastDate: DateTime(2050));
     if (pickedDate != null && pickedDate != currentDate) {
       setState(() {
-        selectedTime = pickedDate;
-        get() => selectedTime;
+        selectedTimeTo = pickedDate;
+        get() => selectedTimeTo;
+
+        if(pickedDate.difference(selectedTimeFrom) >= Duration(days: 0))
+          {
+            DateTimeRange Hi = DateTimeRange(start: selectedTimeFrom, end: pickedDate);
+          }else{
+          _showMyDialog();
+        }
       });
     }
   }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Alert'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('Error with the dates!'),
+                Text(' The last date can not be earlier that the beginning date! '),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Change the dates.'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
 }
 
 
