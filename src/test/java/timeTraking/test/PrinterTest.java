f988ab4cc2b55e660e3044c1ce3906c0b7d4fb78
1391570 <91088455+1391570@users.noreply.github.com>
2021-11-24 18:13:03 +0100

    Added a tag to test

--- src/test/java/timeTraking/test/PrinterTest.java
+++ src/test/java/timeTraking/test/PrinterTest.java
@@ -26,6 +26,7 @@ public class PrinterTest {
         Project root = new Project("root", null);
         Project p1 = new Project("P1", root);
         Project p2 = new Project("P2", root);
+        Project p3 = new Project("P3", root, "Java");
         Task t1 = new Task("T1", root);
         Task t2 = new Task("T2", p1);
         Task t3 = new Task("T3", p2);
